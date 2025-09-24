f4862114e71131f261fb866a606227a919ce3a67
aryan21231212 <aryanpratapsingh674@gmail.com>
2025-09-24 12:02:18 +0530

    DB connected

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,20 @@
 import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
+import dotenv from "dotenv"
 
 
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
 const PORT = 3000;
 
 //DB
-
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("DB connection error:", err.message));
 
 
 app.listen(PORT,()=>{
